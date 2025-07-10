d46303ef0823c6a0d2ed44510ad6b7e8e44c2228
hamcoding9 <dientes8520@gmail.com>
2025-07-10 09:10:32 +0900

    Add error handling Result wrapper

---
+++ core/domain/src/main/java/com/hamcoding/core/domain/util/Error.kt
@@ -0,0 +1,3 @@
+package com.hamcoding.core.domain.util
+
+interface Error
--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.ui.graphics)
