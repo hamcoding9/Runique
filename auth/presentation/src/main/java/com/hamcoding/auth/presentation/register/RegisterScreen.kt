2509d4c00587789cb9eee1e158a8d3c903fc5d75
hamcoding9 <dientes8520@gmail.com>
2025-08-05 21:19:39 +0900

    feat: implement Password Requirement composable

---
+++ auth/presentation/src/main/java/com/hamcoding/auth/presentation/register/RegisterScreen.kt
@@ -0,0 +1,45 @@
+package com.hamcoding.auth.presentation.register
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hamcoding.core.presentation.designsystem.CheckIcon
+import com.hamcoding.core.presentation.designsystem.CrossIcon
+import com.hamcoding.core.presentation.designsystem.RuniqueDarkRed
+import com.hamcoding.core.presentation.designsystem.RuniqueGreen
+
+@Composable
+fun PasswordRequirement(
+    text: String,
+    isValid: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = if (isValid) {
+                CheckIcon
+            } else {
+                CrossIcon
+            },
+            contentDescription = null,
+            tint = if (isValid) RuniqueGreen else RuniqueDarkRed
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 14.sp
+        )
+    }
+}
